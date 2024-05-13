d8941b1247a903e95ceb3dbf2d4bc85dec712540
Gregor <me@onatcer.com>
2024-05-14 01:32:17 +0200

    change build target

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    target: 'esnext'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
